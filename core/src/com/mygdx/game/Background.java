332c2b26c58873ff506132336fd8404845426621
Arthur <saintgrikus@gmail.com>
2022-04-14 23:12:24 +0300

    changed background

--- core/src/com/mygdx/game/Background.java
+++ core/src/com/mygdx/game/Background.java
@@ -1,14 +1,44 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Background {
-    private Texture tx;
-    private Vector2 pos;
+
+    class BGPicture {
+        private Texture tx;
+        private Vector2 pos;
+
+        public BGPicture(Vector2 pos) {
+            tx = new Texture("back.png");
+            this.pos = pos;
+        }
+    }
+    private int speed;
+    private BGPicture[] backs;
 
     public Background() {
-        tx = new Texture("back.png");
-        pos = new Vector2(0, 0)
+        speed = 2;
+        backs = new BGPicture[2];
+        backs[0] = new BGPicture(new Vector2(0,0));
+        backs[1] = new BGPicture(new Vector2( 800, 0));
+    }
+
+    public void render(SpriteBatch batch) {
+        for (int i = 0; i < backs.length; i++) {
+            batch.draw(backs[i].tx, backs[i].pos.x, backs[i].pos.y);
+        }
+    }
+
+    public void update() {
+        for (int i = 0; i < backs.length; i++) {
+            backs[0].pos.x -= speed;
+        }
+        backs[0].pos.x -= speed;
+        if(backs[0].pos.x < -800) {
+            backs[0].pos.x = 0;
+            backs[1].pos.x = 800;
+        }
     }
 }
