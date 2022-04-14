7faac0d12f2d9561eac7dd1f73d2a389e20f39a1
Arthur <saintgrikus@gmail.com>
2022-04-15 01:24:11 +0300

    fixed obstacles

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,12 +8,14 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Background bg;
 	Bird bird;
+	Obstacles obstacles;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		bg = new Background();
 		bird = new Bird();
+		obstacles = new Obstacles();
 	}
 
 	@Override
@@ -23,12 +25,14 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.begin();
 		bg.render(batch);
 		bird.render(batch);
+		obstacles.render(batch);
 		batch.end();
 	}
 
 	public void update() {
 		bg.update();
 		bird.update();
+		obstacles.update();
 	}
 	
 	@Override
--- core/src/com/mygdx/game/Obstacles.java
+++ core/src/com/mygdx/game/Obstacles.java
@@ -4,18 +4,25 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Random;
+
 public class Obstacles {
     class WallPair {
         Vector2 position;
         float speed;
+        int offset;
 
         public WallPair(Vector2 pos) {
             position = pos;
-            speed = 2;
+            speed = 4;
+            offset = new Random().nextInt(250);
         }
 
         public void update() {
             position.x -= speed;
+            if(position.x <- 50) {
+                position.x = 1280;
+            }
         }
     }
 
@@ -37,8 +44,13 @@ public class Obstacles {
     public void render(SpriteBatch batch) {
         for (int i = 0; i < obs.length; i++) {
             batch.draw(txt, obs[i].position.x, obs[i].position.y);
+            batch.draw(txt, obs[i].position.x, obs[i].position.y + betweenDistance + txt.getHeight() - obs[i].offset);
         }
     }
 
-    public void update
+    public void update() {
+        for (int i = 0; i < obs.length; i++) {
+            obs[i].update();
+        }
+    }
 }
